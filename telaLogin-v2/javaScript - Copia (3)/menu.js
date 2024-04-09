7a87d25b29a26df1b977a99d59ef0786028bdc45
ViniciusLeopoldino <135030968+ViniciusLeopoldino@users.noreply.github.com>
2024-04-08 23:09:11 -0300

    atualização

---
+++ telaLogin-v2/javaScript - Copia (3)/menu.js
@@ -0,0 +1,13 @@
+function changeContent(contentId) {
+    var contents = document.getElementsByClassName('conteudo');
+    for (var i = 0; i < contents.length; i++) {
+        contents[i].style.display = 'none';
+    }
+    document.getElementById(contentId).style.display = 'block';
+}
+
+function logout() {
+    // Fazer logout, redirecionar para a página de login
+    window.location.href = "login.html";
+}
+
